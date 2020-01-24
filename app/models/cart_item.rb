74a6beab116eee3811199ebdffbc97d3d24cc4aa
15765-akaha <turning2012@gmail.com>
2020-01-24 14:24:34 +0000

    モデルのvalidation追加設定

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,5 +3,4 @@ class CartItem < ApplicationRecord
    # 数量0以下に変更して保存されないように
    belongs_to :customer
    belongs_to :items
-   #？？？上の場合は記述をまとめることできる？？？
 end
--- app/models/ship_address.rb
+++ app/models/ship_address.rb
@@ -1,3 +1,8 @@
 class ShipAddress < ApplicationRecord
+   validates :last_name,  presence: true
+   validates :first_name, presence: true
+   validates :post_code,  presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+   # 郵便番号のフォーマット指定 ３桁の数字 + ハイフン + ４桁の数字 Viewのフォーム設定
+   validates :address, presence: true
    belongs_to :customer
 end
