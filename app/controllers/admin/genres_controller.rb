fefdb9c3e7a1cf7b3ab2c07eafa40c6cbd3859d1
USER <kanon.ueno1117@gmail.com>
2020-01-25 12:44:29 +0000

    ジャンル登録フォームを追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,22 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
   end
 
   def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to admin_genres_path
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name, :is_enabled)
   end
 end
