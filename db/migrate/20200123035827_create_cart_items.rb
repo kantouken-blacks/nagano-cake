50d4a337455075487520520f5e02dacac881922c
USER <kanon.ueno1117@gmail.com>
2020-01-25 05:56:01 +0000

    migrateファイルのsyntax error修正

--- db/migrate/20200123035827_create_cart_items.rb
+++ db/migrate/20200123035827_create_cart_items.rb
@@ -1,11 +1,11 @@
 class CreateCartItems < ActiveRecord::Migration[5.2]
   def change
     create_table :cart_items do |t|
-      t.integer :customer_id	null: false, default: ""
+      t.integer :customer_id, null: false, default: ""
       # 外部キー 会員のID
-      t.integer :item_id		null: false, default: ""
+      t.integer :item_id, 	null: false, default: ""
       # 外部キー 商品のID
-      t.integer :quantity		null: false, default: ""
+      t.integer :quantity,	null: false, default: ""
 
       t.timestamps
     end
--- db/migrate/20200123040923_create_generes.rb
+++ db/migrate/20200123040923_create_generes.rb
@@ -1,9 +1,9 @@
 class CreateGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :genres do |t|
-      t.string  :name        null: false, default: ""
+      t.string  :name,        null: false, default: ""
 
-      t.boolean :is_enabled default: true, null: false
+      t.boolean :is_enabled,  default: true, null: false
       # 商品の有効/無効 TRUE =有効 FALSE =無効 初期では無効設定
 
       t.timestamps
