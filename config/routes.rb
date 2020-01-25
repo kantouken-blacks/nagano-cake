1f63cd6eb9f1aa47b55acd01442901b753417e4c
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-01-25 04:20:37 +0000

    route 修正

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ devise_for :customers, controllers: {
   #admin
   namespace :admin do
     root 'homes#top'
-    resources :customer_datas, only: [:index, :edit, :update]
+    resources :customer_datas, only: [:show, :index, :edit, :update]
     resources :items, except: [:destroy]
     resources :genres, except: [:new, :show, :destroy]
     resources :orders, only: [:index, :show, :update]
@@ -20,7 +20,7 @@ devise_for :customers, controllers: {
   #customer
   root 'homes#top'
   get '/thanks' => 'homes#thanks'
-  resources :customers, only: [:index, :edit, :update]
+  resources :customers, only: [:edit, :update]
   resources :withdrow, only: [:edit, :update]
   resources :ship_addresses, except: [:new, :show]
   resources :cart_items, except: [:new, :show, :edit]
--- db/migrate/20200123041637_create_orders.rb
+++ db/migrate/20200123041637_create_orders.rb
@@ -11,7 +11,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
       t.integer  :carriage        null: false, default: 800
       t.integer  :total_price     null: false
       # Viewで表示された小計に送料(carriage)をプラスして格納する
-      t.integer  :order_status　 　null: false, default: 0
+      t.integer  :order_status    null: false, default: 0
       # e_num設定 ０= 入金待ち、１= 入金確認、２= 製作中、３= 発送準備中、４= 発送済み 登録時は0(入金待ち)
       # ？？？製作ステータスとのオート機能はモデルで設定するわけではないのか？？？
       t.timestamps
