ef898ec5983bd7722a9ae55e6e1b078eda3fb0b8
USER <kanon.ueno1117@gmail.com>
2020-01-25 09:25:26 +0000

    itemモデルのsyntax_errorを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
 	validates :price, 		  presence: true
 	validates :sale_status, presence: true
 	# ！！with optionでまとめる！！
-	has_many :cart_items dependent: :destroy
+	has_many :cart_items, dependent: :destroy
 	belongs_to :genre
 	attachment :image
 	enum sale_status: [['販売可',0],['販売不可',1]]
