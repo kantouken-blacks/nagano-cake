00b6f5665f7472b1d29752952645064992698732
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-01-25 04:41:17 +0000

    コンフリクト対応 マイグレーションファイルcreate_genres編集

--- db/migrate/20200123040923_create_generes.rb
+++ db/migrate/20200123040923_create_generes.rb
@@ -2,13 +2,10 @@ class CreateGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :genres do |t|
       t.string  :name        null: false, default: ""
-<<<<<<< HEAD
+
       t.boolean :is_enabled default: true, null: false
       # 商品の有効/無効 TRUE =有効 FALSE =無効 初期では無効設定
-=======
-      t.boolean :is_enabled  default: true, null: false
-      # 商品の有効/無効 TRUE =有効　FALSE =無効　初期では無効設定
->>>>>>> 5f38f84171ae66d7c5e46fe9461850fe24b76f79
+
       t.timestamps
     end
   end
