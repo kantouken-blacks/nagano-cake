a9245e34475e802fc1a19e2d4e225ccd2d37d54a
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-01-26 07:50:03 +0000

    session view　ボタンまで表示

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,7 +1,10 @@
 class SessionController < ApplicationController
   def new
+    # ship_address = Ship_address.new
   end
 
   def create
+    # @ship_address = Ship_address.new(ship_address_params)
+    # @ship_address.customer_id = current_user.id
   end
 end
