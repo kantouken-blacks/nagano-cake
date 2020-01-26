da1415cb35e4630775fec1de3e24bb8df66b1bf8
USER <kanon.ueno1117@gmail.com>
2020-01-26 11:28:47 +0000

    enumの記述を変更

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
 	belongs_to :customer
 	has_many   :order_details, dependent: :destroy
-	enum order_status: [['入金待ち',0],['入金確認',1],['製作中',2],['発送準備中',3],['発送済み',4]]
+	enum order_status: [:入金待ち, :入金確認, :製作中, :発送準備中, :発送済み]
 end
