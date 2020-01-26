9c1406e80dfaff33b5cc97d12211c977ce5ec16d
USER <kanon.ueno1117@gmail.com>
2020-01-26 12:25:47 +0000

    admin 注文一覧、詳細ページのアクションを定義

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,31 @@
 class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.all
+    @orders_details = OrderDetail.all
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.find(params[:order])
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order_detail = order.find(paras[:id])
+    if @order.update(order_params)
+      redirect_to admin_order(@order)
+    elsif
+      @order_detail.update(order_detail_params)
+      redirect_to admin_order(@order)
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:order_status)
+  end
+
+  def order_detail_params
+    params.require(:order_detail).permit(:item_status)
   end
 end
