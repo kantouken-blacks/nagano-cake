13eccc9deea526b6951461e15d89f94f18002f4c
USER <kanon.ueno1117@gmail.com>
2020-01-26 04:23:46 +0000

    admin会員情報の編集フォームを追加

--- app/controllers/admin/customer_datas_controller.rb
+++ app/controllers/admin/customer_datas_controller.rb
@@ -7,8 +7,12 @@ class Admin::CustomerDatasController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customer_data_path
   end
 end
