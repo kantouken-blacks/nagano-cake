a99adaf23e58875027c11ec41755d6c70901fab9
USER <kanon.ueno1117@gmail.com>
2020-01-26 01:47:04 +0000

    ジャンルの一覧テーブルを追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,6 +1,7 @@
 class Admin::GenresController < ApplicationController
   def index
     @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
@@ -10,9 +11,13 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admin_genres_path
   end
 
   private
