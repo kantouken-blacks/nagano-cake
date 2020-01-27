6f3ed9fd01912b98973eef2330b7189933485c38
15765-akaha <turning2012@gmail.com>
2020-01-27 07:16:02 +0000

    shipaddressesコントローラーのindexアクションを定義

--- app/controllers/ship_addresses_controller.rb
+++ app/controllers/ship_addresses_controller.rb
@@ -1,5 +1,8 @@
 class ShipAddressesController < ApplicationController
   def index
+    @shipaddress = Shipaddress.new
+    @shipaddress = Shipaddress.find(params[:id])
+    @shipaddresses = @customer.shipaddress
   end
 
   def create
