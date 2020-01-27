b8bc0f0d35824185e960fcdd808c86c333be64bb
15765-akaha <turning2012@gmail.com>
2020-01-27 08:17:11 +0000

    post_code ハイフン無し7桁数字固定 validatesに変更

--- app/models/ship_address.rb
+++ app/models/ship_address.rb
@@ -1,8 +1,8 @@
 class ShipAddress < ApplicationRecord
    validates :last_name,  presence: true
    validates :first_name, presence: true
-   validates :post_code,  presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
-   # 郵便番号のフォーマット指定 ３桁の数字 + ハイフン + ４桁の数字 Viewのフォーム設定
+   validates :post_code,  presence: true, format: {with: /\A\d{7}\z/}
+   # 郵便番号のフォーマット指定 ハイフン無し７桁固定 Viewのフォーム設定
    validates :address, presence: true
    belongs_to :customer
 end
