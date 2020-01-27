a379fe1e252b9214d2574bdb277456b7b66c1f0d
15765-akaha <turning2012@gmail.com>
2020-01-27 01:32:00 +0000

    customerコントーラー edit updateアクション定義とストロングパラメーター設定

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,8 +4,17 @@ class CustomersController < ApplicationController
   end
 
   def edit
+  	@customer = Customer.find(params[:id])
   end
 
   def update
+  	customer = Customer.find(params[:id])
+  	customer.update(params[:id])
+  	redirect_to customer_path(customer)
+  end
+  private
+  def customer_params
+  	  params.require(:customer).permit(:is_enabled, :last_name, :first_name, :last_name_kana, :first_name_kana,
+  	                                   :phone_number, :email, :password, :post_code, :address)
   end
 end
