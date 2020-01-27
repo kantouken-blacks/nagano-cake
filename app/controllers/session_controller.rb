1a635ad4fc5c29cc80bbfec8bbb1c22aa3391e75
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-01-27 08:36:43 +0000

    注文履歴一覧画面　作成

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,16 @@
 class SessionController < ApplicationController
   def new
-    # @ship_address = Ship_address.new
+    @ship_address = Ship_address.new
+    @order = Order.new
   end
 
   def create
-    # @ship_address = Ship_address.new(ship_address_params)
-    # @ship_address.customer_id = current_user.id
+    @ship_address = Ship_address.new(ship_address_params)
+    @ship_address.save
   end
+
+  private
+   def ship_address_params
+     params.require(:ship_address).permit(:last_name, :first_name, :post_code, :address)
+   end
 end
