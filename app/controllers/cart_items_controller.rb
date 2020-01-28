b759dcdd86f883f6cdbc5317d23c50721a85d19b
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-01-28 06:00:44 +0000

    cart_items_controller,items_controller　編集

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,6 +4,9 @@ class CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = CartItem.build(cart_item_params)
+    @cart_item.save
+    redirect_to cart_items_path
   end
 
   def update
@@ -13,5 +16,10 @@ class CartItemsController < ApplicationController
     item = Item.find(params[:id])
     item.destroy
     redirect_to cart_items_path
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:customer_id, :item_id, :quantity)
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,8 +8,9 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @cart_item = CartItem.new
   	@genres = Genre.all
   	@item = Item.find(params[:id])
   end
-  
+
 end
