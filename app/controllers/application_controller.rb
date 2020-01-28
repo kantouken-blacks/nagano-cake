f9272a02d4d4a8b419b601c78fbfa1617eac61eb
USER <kanon.ueno1117@gmail.com>
2020-01-28 12:27:54 +0000

    会員ログアウト後の遷移先を編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :admin
       new_admin_session_path
+    else
+      root_path
     end
   end
 end
