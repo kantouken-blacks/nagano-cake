446275c99d01186897b56829386cd129673c7c1b
USER <kanon.ueno1117@gmail.com>
2020-01-28 08:35:51 +0000

    admin 会員編集ページのエラー解消、デザイン編集

--- app/controllers/admin/customer_datas_controller.rb
+++ app/controllers/admin/customer_datas_controller.rb
@@ -11,8 +11,17 @@ class Admin::CustomerDatasController < ApplicationController
   end
 
   def update
-    customer = Customer.find(params[:id])
-    customer.update(customer_params)
-    redirect_to admin_customer_data_path
+    @customer = Customer.find(params[:id])
+    # binding.pry
+    if @customer.update(customer_params)
+      redirect_to admin_customer_data_path
+    else
+      redirect_to edit_admin_customer_data_path(@customer)
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email, :is_enabled)
   end
 end
