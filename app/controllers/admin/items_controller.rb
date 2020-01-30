af23fc19d7dd50827ea33a79d406e37123ec2ae5
USER <kanon.ueno1117@gmail.com>
2020-01-30 09:03:17 +0000

    admin 商品新規作成後のリダイレクト先を修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -17,7 +17,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to admin_item_path(@item)
+    redirect_to admin_items_path
   end
 
   def edit
