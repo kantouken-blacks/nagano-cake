b0b1daf39cd0bb16b457567685191cf31ee5a4e4
15765-akaha <turning2012@gmail.com>
2020-01-30 09:25:52 +0000

    販売可能の商品のみを商品一覧に表示する記述に変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,10 @@ class ItemsController < ApplicationController
 
   def index
   	@genres = Genre.all
-  	@items = Item.all.order(genre_id: "asc")
+    @items = Item.where(sale_status: "販売可").order(genre_id: "asc")
+    # 販売ステータスが販売可のみの商品を参照　ジャンル作成の昇順
+
+  	# @items = Item.all.order(genre_id: "asc")
   end
 
   def show
