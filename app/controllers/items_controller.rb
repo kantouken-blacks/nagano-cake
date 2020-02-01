2100d1a5de6a7e68df43ae724bcfd7f5e1f7abce
15765-akaha <turning2012@gmail.com>
2020-02-01 07:27:02 +0000

    カテゴリー商品に紐つくページ一覧ページのタイトルと商品登録数の動的表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,13 +9,13 @@ class ItemsController < ApplicationController
     if params[:genre_id]
       # その[:genre_id]のデータをGenreから@genreに格納
       @genre = Genre.find(params[:genre_id])
-      # @genreに紐付いた商品を全て引っ張る（作成順の昇順）
-      @items = @genre.items.order(created_at: :desc).all
+      # @genreに紐付いた商品で販売可の商品を引っ張る（作成順の昇順）
+      @items = @genre.items.order(created_at: :desc).where(sale_status: "販売可")
 
-      # リクエストがなければ
+      # 含まれていなければ
     else
       @items = Item.where(sale_status: "販売可").order(genre_id: "asc")
-     # 販売ステータスが販売可のみの商品を参照　ジャンル作成の昇順
+     # 販売ステータスが販売可のみの商品を参照 ジャンル作成の昇順
     end
   end
 
