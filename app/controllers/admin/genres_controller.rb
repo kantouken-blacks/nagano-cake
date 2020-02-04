b702599b12a514574fc68eed0a9b745a8f5a434c
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-05 01:51:53 +0900

    コメント追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -24,6 +24,7 @@ class Admin::GenresController < ApplicationController
   def update
     genre = Genre.find(params[:id])
     if genre.update(genre_params)
+      # Model:Genres.rb after_update => 無効にしたら関連商品の販売ステータスを販売不可に変更
       redirect_to admin_genres_path
     else
       flash[:genre_updated_error] = "ジャンル名を入力してください"
