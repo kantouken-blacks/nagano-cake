44ac885857a508b9c62d37d44059e6d7929ad239
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-04 16:44:12 +0900

    admin ジャンルのステータスが無効になったら商品の販売ステータスが自動で販売不可になるように設定

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,13 @@
 class Genre < ApplicationRecord
 	validates :name, presence: true
 	has_many :items
+
+	after_update do
+		items = Genre.find(id).items
+		if (is_enabled) == true
+			items.each {|item|
+				item.update(sale_status: "販売不可")
+			}
+		end
+	end
 end
