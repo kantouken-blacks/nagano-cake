79ad34d8db4ba7efd29a72182494651132ced435
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-04 12:48:35 +0900

    admin 注文一覧のページネションを設定

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,18 +3,18 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
+    if params[:id] # 会員詳細から来た場合
+      @orders = Customer.find(params[:id]).orders.page(params[:page]).per(10)
 
+    elsif request.fullpath.include? "today" # TOP本日受注分から来た場合
+      @orders = Order.where(created_at:  Time.zone.now.all_day).page(params[:page]).per(10)
 
-    if params[:id]
-      @orders = Customer.find(params[:id]).orders
-    elsif request.fullpath.include? "today"
-      @orders = Order.where(created_at:  Time.zone.now.all_day)  
-    elsif request.fullpath.include? "yesterday"
-      @orders = Order.where(created_at: 1.day.ago.all_day)
+    elsif request.fullpath.include? "yesterday" # TOP本日製作分から来た場合
+      @orders = Order.where(created_at: 1.day.ago.all_day).page(params[:page]).per(10)
+
     else
-      @orders = Order.all
+      @orders = Order.page(params[:page]).per(10)
     end
-    # @orders = Order.page(params[:page]).per(10)
   end
 
   def show
